40311d404513b3e4d0c8cc0a339b4290ec63cc12
perossing <perossing@gmail.com>
2018-02-04 14:32:20 -0800

    change folders & paths for cms previews

--- themes/src/cms-previews/cms.js
+++ themes/src/cms-previews/cms.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-import PagePreview from "./cms-preview-templates/pagePreview";
+import PagePreview from "./pagePreview";
 
 CMS.registerPreviewStyle("/css/cms-preview.css");
 CMS.registerPreviewTemplate("pages", PagePreview);
