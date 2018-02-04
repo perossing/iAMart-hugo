2fc04098ef9f853dcb36eb4fd0e68d389e440d70
perossing <perossing@gmail.com>
2018-02-04 14:37:22 -0800

    rearrange preview files

--- themes/src/js/cms.js
+++ themes/src/js/cms.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-import PagePreview from "./pagePreview";
+import PagePreview from "./cms-previews/pagePreview";
 
 CMS.registerPreviewStyle("/css/cms-preview.css");
 CMS.registerPreviewTemplate("pages", PagePreview);
