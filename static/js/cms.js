9c8c2caf3e2471ad5cae27c16095d1ea39f91054
perossing <perossing@gmail.com>
2018-02-04 14:16:00 -0800

    refine script for custom preview

--- static/js/cms.js
+++ static/js/cms.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-import PagePreview from "./cms-preview-templates/pagePreview";
+// import PagePreview from "./cms-preview-templates/pagePreview";
 // import ProductsPreview from "./cms-preview-templates/products";
 
 
 CMS.registerPreviewStyle("/css/styles.css");
-CMS.registerPreviewTemplate("content", PagePreview);
+CMS.registerPreviewTemplate("pages", PagePreview);
 // CMS.registerPreviewTemplate("products", ProductsPreview);
 
 
