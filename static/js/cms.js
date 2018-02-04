88eacdfcafb97de2084e1bd7ff18592f353c32c4
perossing <perossing@gmail.com>
2018-02-04 13:55:32 -0800

    set up custom page preview - start

--- static/js/cms.js
+++ static/js/cms.js
@@ -1,12 +1,13 @@
 import React from "react";
 import CMS from "netlify-cms";
 
-// import PostPreview from "./cms-preview-templates/post";
+import PagePreview from "./cms-preview-templates/pagePreview";
 // import ProductsPreview from "./cms-preview-templates/products";
 
-// console.log("hi monkey")
 
 CMS.registerPreviewStyle("/css/styles.css");
-// CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("content", PagePreview);
 // CMS.registerPreviewTemplate("products", ProductsPreview);
+
+
 
---
+++ static/js/pagePreview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import format from "date-fns/format";
+
+export default class PagePreview extends React.Component {
+  render() {
+    const {entry, widgetFor, getAsset} = this.props;
+
+    <div class="page-content">
+    <h1>{ entry.getIn(["data", "title"])}</h1>
+    <div>{ widgetFor("content") }</div>
+</div>   
+  }
+}
