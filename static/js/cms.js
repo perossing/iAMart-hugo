c08dc4f2603dc737796d3e21582c990061b1c7d5
perossing <perossing@gmail.com>
2018-02-04 11:47:03 -0800

    begin custom cms styling setup

---
+++ static/js/cms.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CMS from "netlify-cms";
+
+// import PostPreview from "./cms-preview-templates/post";
+// import ProductsPreview from "./cms-preview-templates/products";
+
+console.log("hi monkey")
+
+CMS.registerPreviewStyle("/css/styles.css");
+// CMS.registerPreviewTemplate("post", PostPreview);
+// CMS.registerPreviewTemplate("products", ProductsPreview);
+
