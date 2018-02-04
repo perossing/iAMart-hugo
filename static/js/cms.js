c7653be0f136b7cdb5d39da5995d43b73a68b613
perossing <perossing@gmail.com>
2018-02-04 13:35:21 -0800

    resume cms custom preview template setup

--- static/js/cms.js
+++ static/js/cms.js
@@ -4,7 +4,7 @@ import CMS from "netlify-cms";
 // import PostPreview from "./cms-preview-templates/post";
 // import ProductsPreview from "./cms-preview-templates/products";
 
-console.log("hi monkey")
+// console.log("hi monkey")
 
 CMS.registerPreviewStyle("/css/styles.css");
 // CMS.registerPreviewTemplate("post", PostPreview);
